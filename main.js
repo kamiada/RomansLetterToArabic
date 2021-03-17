cd7fc4167ab91e898d6e4dbb2f5c8b5d5cfe4b18
kamiada <kamiada528@gmail.com>
2021-03-17 00:22:51 +0000

    roman numbers added

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
+const roman = {
+    I = 1,
+    V = 5,
+    X = 10,
+    L = 50,
+    C = 100,
+    D = 500,
+    M = 1000,
+}
+
+
 const interface = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
